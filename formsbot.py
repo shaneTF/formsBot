c91c020928f239f30a0de38d04614c18171b93a3
Shane Franklin <shanefranklin148@gmail.com>
2023-01-21 18:44:53 -0500

    Changed so that modal appears on slash command run instead with button activation

--- formsbot.py
+++ formsbot.py
@@ -7,15 +7,9 @@ load_dotenv()
 bot = lightbulb.BotApp(token=f"{os.getenv('discord_token')}")
 miru.install(bot)
 
-class ModalView(miru.View):
-    @miru.button(label="Modal", style=hikari.ButtonStyle.SECONDARY)
-    async def modal_button(self, button: miru.Button, ctx: miru.ViewContext) -> None:
-        modal = Modal(title="Title")
-        await ctx.respond_with_modal(modal)
-
-class Modal(miru.Modal):
+class TestModal(miru.Modal):
     name = miru.TextInput(label="Name", placeholder="Your Name", required=True)
-    bio = miru.TextInput(label="Bio", value="Pre-filled content", style=hikari.TextInputStyle.PARAGRAPH)
+    bio = miru.TextInput(label="Bio", placeholder="Say a little something about yourself!", style=hikari.TextInputStyle.PARAGRAPH)
 
     async def callback(self, ctx: miru.ModalContext) -> None:
         await ctx.respond(f"Your name: ```{self.name.value}```\nYour bio: ```{self.bio.value}```")
@@ -24,9 +18,8 @@ class Modal(miru.Modal):
 @bot.command
 @lightbulb.command('firstform', 'Produces button and button will produce modal')
 @lightbulb.implements(lightbulb.SlashCommand)
-async def firstForm(ctx):
-    view = ModalView()
-    message = await ctx.respond("Test message", components=view)
-    await view.start(message)
+async def firstForm(ctx: lightbulb.Context) -> None:
+    modal = TestModal(title="Tell us about yourself!")
+    await modal.send(ctx.interaction)
 
 bot.run()
