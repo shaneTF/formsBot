985002abd932f988e1fb57d5d8ad26b970ae161c
Shane Franklin <shanefranklin148@gmail.com>
2023-01-22 14:49:13 -0500

    Changed messages for feedback form. Removed unused code.

--- formsbot.py
+++ formsbot.py
@@ -6,20 +6,21 @@ load_dotenv()
 
 bot = lightbulb.BotApp(token=f"{os.getenv('discord_token')}")
 miru.install(bot)
-
+# What happens if you create an embed and add fields with callback?
+# Maybe try returning input fields with just return not ctx.respond.
 class TestModal(miru.Modal):
-    name = miru.TextInput(label="Name", placeholder="Your Name", required=True)
-    bio = miru.TextInput(label="Bio", placeholder="Say a little something about yourself!", style=hikari.TextInputStyle.PARAGRAPH)
+    name = miru.TextInput(label="Username", placeholder="Your Username", required=True)
+    feedBack = miru.TextInput(label="Feedback", placeholder="What do you think of the bot? Any improvements?", style=hikari.TextInputStyle.PARAGRAPH)
 
     async def callback(self, ctx: miru.ModalContext) -> None:
-        await ctx.respond(f"Your name: ```{self.name.value}```\nYour bio: ```{self.bio.value}```")
-
+        print(f"Name: {self.name.value}, feedback: {self.feedBack.value}, Input3: {self.input3.value}")
+        await ctx.respond(f"```You feedback has been recorded!```")
 
 @bot.command
-@lightbulb.command('firstform', 'Produces button and button will produce modal')
+@lightbulb.command('feedback', 'Produces modal for feedback')
 @lightbulb.implements(lightbulb.SlashCommand)
-async def firstForm(ctx: lightbulb.Context) -> None:
-    modal = TestModal(title="Tell us about yourself!")
+async def feedback(ctx: lightbulb.Context) -> None:
+    modal = TestModal(title="Tell us your feedback!")
     await modal.send(ctx.interaction)
 
 bot.run()
