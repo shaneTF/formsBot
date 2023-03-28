a2f3fd798b22663b19b18220fd8949df766a8446
Shane Franklin <shanefranklin148@gmail.com>
2023-03-28 14:17:45 -0400

    Fixed report order and added options

--- formsbot.py
+++ formsbot.py
@@ -8,7 +8,8 @@ import json
 
 load_dotenv()
 
-bot = lightbulb.BotApp(token=os.environ['API_KEY'])
+bot = lightbulb.BotApp(token=f"{os.getenv('discord_token')}")
+# bot = lightbulb.BotApp(token=os.environ['API_KEY'])
 miru.install(bot)
 
 field_names = ['Username', 'Feedback']
@@ -18,7 +19,7 @@ class TestModal(miru.Modal):
 
     async def callback(self, ctx: miru.ModalContext) -> None:
         requests.post('https://sheetdb.io/api/v1/rwjtxn150grj6', json={
-            'id': 'INCREMENT',
+            'date': 'DATETIME',
             'name': self.name.value,
             'feedback': self.feedBack.value
         })
@@ -35,18 +36,26 @@ async def feedback(ctx: lightbulb.Context) -> None:
 @bot.command
 @lightbulb.add_checks(lightbulb.has_roles(1053446321186553931) | lightbulb.owner_only)
 @lightbulb.option('limit', 'Limit of results returned.', default='5', type=int)
+@lightbulb.option('date', 'Date of feedback. Format=Year-month-day, i.e. 2023-03-28', type=str, required=False)
 @lightbulb.command('report', 'This will print out the available rows in the feedback spreadsheet.')
 @lightbulb.implements(lightbulb.SlashCommand)
 async def report(ctx):
-    reportData = requests.get('https://sheetdb.io/api/v1/rwjtxn150grj6', params={'limit': ctx.options.limit}).json()
+    reportData = requests.get('https://sheetdb.io/api/v1/rwjtxn150grj6', params={'sort_by': 'date', 'sort_order': 'desc', 'limit': ctx.options.limit}).json()
 
     embedmsg = hikari.Embed(
         title='**Feedback Report**',
         description='~~----------------------~~'
     )
-    for i in range(len(reportData)):
-        embedmsg.add_field(name=reportData[i]['name'], value=reportData[i]['feedback'])
+    print(reportData)
+    if ctx.options.date == None:
+         for i in range(len(reportData)):
+            embedmsg.add_field(name=reportData[i]['name'], value=reportData[i]['feedback'])
 
+    else:
+        for i in range(len(reportData)):
+            if ctx.options.date in reportData[i]['date']:
+                embedmsg.add_field(name=reportData[i]['name'], value=reportData[i]['feedback'])
+       
     await ctx.respond(embedmsg)
 
 bot.run()
