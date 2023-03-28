7ee6fc6f9b69232d1b9c6ed79daf4fce29545074
Shane Franklin <shanefranklin148@gmail.com>
2023-03-28 12:32:37 -0400

    Changed storage to google sheets, and added report command

--- formsbot.py
+++ formsbot.py
@@ -1,7 +1,10 @@
-import hikari, lightbulb, miru
+import hikari, miru
+import lightbulb
 import os
 from dotenv import load_dotenv
 import csv
+import requests
+import json
 
 load_dotenv()
 
@@ -14,10 +17,11 @@ class TestModal(miru.Modal):
     feedBack = miru.TextInput(label="Feedback", placeholder="What do you think of the bot? Any improvements?", style=hikari.TextInputStyle.PARAGRAPH)
 
     async def callback(self, ctx: miru.ModalContext) -> None:
-        feedbackDict = {"Username": f"{self.name.value}", "Feedback": f"{self.feedBack.value}"}
-        with open('feedbackDB.csv', 'a') as csv_file:
-            dict_object = csv.DictWriter(csv_file, fieldnames=field_names, lineterminator='\n')
-            dict_object.writerow(feedbackDict)
+        requests.post('https://sheetdb.io/api/v1/rwjtxn150grj6', json={
+            'id': 'INCREMENT',
+            'name': self.name.value,
+            'feedback': self.feedBack.value
+        })
 
         await ctx.respond('Your feedback has been recorded! Thanks!')
 
@@ -27,5 +31,22 @@ class TestModal(miru.Modal):
 async def feedback(ctx: lightbulb.Context) -> None:
     modal = TestModal(title="Tell us your feedback!")
     await modal.send(ctx.interaction)
+
+@bot.command
+@lightbulb.add_checks(lightbulb.has_roles(1053446321186553931) | lightbulb.owner_only)
+@lightbulb.option('limit', 'Limit of results returned.', default='5', type=int)
+@lightbulb.command('report', 'This will print out the available rows in the feedback spreadsheet.')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def report(ctx):
+    reportData = requests.get('https://sheetdb.io/api/v1/rwjtxn150grj6', params={'limit': ctx.options.limit}).json()
+
+    embedmsg = hikari.Embed(
+        title='**Feedback Report**',
+        description='~~----------------------~~'
+    )
+    for i in range(len(reportData)):
+        embedmsg.add_field(name=reportData[i]['name'], value=reportData[i]['feedback'])
+
+    await ctx.respond(embedmsg)
 
 bot.run()
